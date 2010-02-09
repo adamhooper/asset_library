c68e3c7f0c9ed078ead5c02f7157d177ac249935
George Ogata <george.ogata@gmail.com>
2010-02-09 01:02:48 -0500

    Add compiler factory.

---
+++ spec/asset_library/compiler_spec.rb
@@ -0,0 +1,33 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe(AssetLibrary::Compiler) do
+  Compiler = AssetLibrary::Compiler
+
+  describe('.create') do
+    it('should return a Default compiler for :default') do
+      Compiler.create(:default).should be_a(Compiler::Default)
+    end
+
+    it('should return a Closure compiler for :closure') do
+      Compiler.create(:closure, :closure_path => '').should be_a(Compiler::Closure)
+    end
+
+    it('should pass the configuration to the compiler') do
+      Compiler.create(:default, {:param => 2}).config[:param].should == 2
+    end
+  end
+
+  describe('.register') do
+    after(:each) do
+      Compiler.reset!
+    end
+
+    TestCompiler = Class.new(Compiler::Base)
+
+    it('should register a custom compiler type') do
+      Compiler.register(:test, TestCompiler)
+      compiler = Compiler.create(:test)
+      compiler.should be_a(TestCompiler)
+    end
+  end
+end
